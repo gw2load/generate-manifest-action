cf7d11a365c1a734922b578ab30295a566eda1f4
darthmaim <git@darthmaim.de>
2025-07-03 16:27:04 +0200

    Log messages in `@actions/core` mocks (#44)

--- __fixtures__/core.ts
+++ __fixtures__/core.ts
@@ -1,10 +1,10 @@
 import * as core from '@actions/core'
 import { jest } from '@jest/globals'
 
-export const debug = jest.fn<typeof core.debug>()
-export const error = jest.fn<typeof core.error>()
-export const info = jest.fn<typeof core.info>()
+export const debug = jest.fn<typeof core.debug>(console.debug)
+export const error = jest.fn<typeof core.error>(console.error)
+export const info = jest.fn<typeof core.info>(console.info)
 export const getInput = jest.fn<typeof core.getInput>(core.getInput)
 export const setOutput = jest.fn<typeof core.setOutput>()
 export const setFailed = jest.fn<typeof core.setFailed>()
-export const warning = jest.fn<typeof core.warning>()
+export const warning = jest.fn<typeof core.warning>(console.error)
